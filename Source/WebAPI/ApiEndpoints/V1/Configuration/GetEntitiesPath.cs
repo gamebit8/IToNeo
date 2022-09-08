a7f5bfc765478ec371034b926a2a7cb242ad7355
Ovchinnikov Anton <gamebit8@gmail.com>
2022-09-08 12:45:53 +0400

    Rename field and property names

---
+++ Source/WebAPI/ApiEndpoints/V1/Configuration/GetEntitiesPath.cs
@@ -0,0 +1,61 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace IToNeo.WebAPI.ApiEndpoints.V1.Configuration
+{
+    [Route("api/v1/configurations")]
+    [ApiController]
+    [AllowAnonymous]
+    public class GetEntitiesPaths : EndpointBaseSync.WithoutRequest.WithActionResult<GetEntitiesPathsResult>
+    {
+        [HttpGet("paths")]
+        [SwaggerOperation(
+            Summary = "Get a Paths",
+            Description = "Get a Paths",
+            OperationId = "GetPaths",
+            Tags = new[] { "Configuration" })
+        ]
+        public override ActionResult<GetEntitiesPathsResult> Handle()
+        {
+            GetEntitiesPathsResult result = GetConfigurationUrlsResult();
+            return Ok(result);
+        }
+
+        private GetEntitiesPathsResult GetConfigurationUrlsResult()
+        {
+            return new GetEntitiesPathsResult()
+            {
+                EmployeesPath = Url.Action("Handle", "GetEmployees", null),
+                EquipmentsPath = Url.Action("Handle", "GetEquipments", null),
+                EquipmentTypesPath = Url.Action("Handle", "GetEquipmentTypes", null),
+                EquipmentStatusesPath = Url.Action("Handle", "GetEquipmentStatuses", null),
+                SoftwaresPath = Url.Action("Handle", "GetSoftwares", null),
+                SoftwareDevelopersPath = Url.Action("Handle", "GetDevelopers", null),
+                SoftwareLicensesPath = Url.Action("Handle", "GetSoftwareLicenses", null),
+                SoftwareLicenseTypesPath = Url.Action("Handle", "GetSoftwareLicenseTypes", null),
+                DisposalsPath = Url.Action("Handle", "GetEquipmentDisposals", null),
+                OrganizationsPath = Url.Action("Handle", "GetOrganizations", null),
+                PlacesPath = Url.Action("Handle", "GetPlaces", null),
+                WriteOffsPath = Url.Action("Handle", "GetEquipmentWriteOffs", null),
+                AuthenticatePath = Url.Action("Handle", "Authenticate", null),
+                RegisterPath = Url.Action("Handle", "Register", null),
+                ChangePasswordPath = Url.Action("Handle", "ChangePassword", null),
+                ChangePasswordAfterResetPath = Url.Action("Handle", "ChangePasswordAfterReset", null),
+                RecoveryPasswordPath = Url.Action("Handle", "ResetPassword", null),
+                ConfirmEmailPath = Url.Action("Handle", "ConfirmEmail", null),
+                EmployeeSearchHelperPath = Url.Action("Handle", "GetEmployeesShort", null),
+                OrganizationSearchHelperPath = Url.Action("Handle", "GetOrganizationsShort", null),
+                EquipmentSearchHelperPath = Url.Action("Handle", "GetEquipmentsShort", null),
+                EquipmentTypeSearchHelperPath = Url.Action("Handle", "GetEquipmentTypesShort", null),
+                EquipmentStatusSearchHelperPath = Url.Action("Handle", "GetEquipmentStatusesShort", null),
+                PlaceSearchHelperPath = Url.Action("Handle", "GetPlacesShort", null),
+                FilesPath = Url.Action("Handle", "GetFiles", null),
+                EquipmentsSoftwareLicensesPath = Url.Action("Handle", "GetEquipmentsSoftwareLicenses", null),
+                UsersPath = Url.Action("Handle", "GetUsers", null),
+                UserRolesPath = Url.Action("Handle", "GetUserRoles", null)
+            };
+        }
+    }
+}
---
+++ Source/WebAPI/ApiEndpoints/V1/Configuration/GetEntitiesPathsResult.cs
@@ -0,0 +1,34 @@
+﻿namespace IToNeo.WebAPI.ApiEndpoints.V1.Configuration
+{
+    public class GetEntitiesPathsResult
+    {
+        public string EmployeesPath { get; set; }
+        public string EquipmentsPath { get; set; }
+        public string EquipmentTypesPath { get; set; }
+        public string EquipmentStatusesPath { get; set; }
+        public string SoftwaresPath { get; set; }
+        public string SoftwareDevelopersPath { get; set; }
+        public string SoftwareLicensesPath { get; set; }
+        public string SoftwareLicenseTypesPath { get; set; }
+        public string DisposalsPath { get; set; }
+        public string OrganizationsPath { get; set; }
+        public string PlacesPath { get; set; }
+        public string WriteOffsPath { get; set; }
+        public string AuthenticatePath { get; set; }
+        public string RegisterPath { get; set; }
+        public string ChangePasswordPath { get; set; }
+        public string ChangePasswordAfterResetPath { get; set; }
+        public string RecoveryPasswordPath { get; set; }
+        public string ConfirmEmailPath { get; set; }
+        public string FilesPath { get; set; }
+        public string EmployeeSearchHelperPath { get; set; }
+        public string OrganizationSearchHelperPath { get; set; }
+        public string EquipmentSearchHelperPath { get; set; }
+        public string EquipmentTypeSearchHelperPath { get; set; }
+        public string EquipmentStatusSearchHelperPath { get; set; }
+        public string EquipmentsSoftwareLicensesPath { get; set; }
+        public string PlaceSearchHelperPath { get; set; }
+        public string UsersPath { get; set; }
+        public string UserRolesPath { get; set; }
+    }
+}
